50e9d98bcc2956602bd5c8491a294d00de55481d
0x.acessoria <aecioemailnet@gmail.com>
2026-02-17 21:59:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "SUA_API_KEY";
+const API_KEY = "9bbfd5e96b1e23a705a8fc51d36dc8e1";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w300";
 
