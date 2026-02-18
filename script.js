6736009ff6e97e4e5df62211354d05545f15e8fe
0x.acessoria <aecioemailnet@gmail.com>
2026-02-17 22:17:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "SUA_API_KEY";
+const API_KEY = "9bbfd5e96b1e23a705a8fc51d36dc8e1";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w300";
 
@@ -6,15 +6,20 @@ function fetchCategory(endpoint, containerId) {
   fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=pt-BR`)
     .then(res => res.json())
     .then(data => {
-      console.log(data); // mostra no console
       const container = document.getElementById(containerId);
+      container.innerHTML = "";
+
       data.results.forEach(movie => {
         const div = document.createElement("div");
-        div.innerHTML = `<p>${movie.title}</p>`;
+        div.classList.add("card");
+        div.innerHTML = `
+          <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+        `;
         container.appendChild(div);
       });
-    })
-    .catch(err => console.error("Erro:", err));
+    });
 }
 
 fetchCategory("/movie/popular", "popular");
+fetchCategory("/movie/now_playing", "now_playing");
+fetchCategory("/movie/top_rated", "top_rated");
