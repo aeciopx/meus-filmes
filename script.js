c8f7ab0a1c89bfd9324c5eeab2b5459547922abb
0x.acessoria <aecioemailnet@gmail.com>
2026-02-18 21:08:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const API_KEY = "9bbfd5e96b1e23a705a8fc51d36dc8e1";
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w300";
+const apiKey = "9bbfd5e96b1e23a705a8fc51d36dc8e1";
+const baseUrl = "https://api.themoviedb.org/3";
+const imageUrl = "https://image.tmdb.org/t/p/w500";
 
-function fetchCategory(endpoint, containerId) {
-  fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=pt-BR`)
-    .then(res => res.json())
-    .then(data => {
-      const container = document.getElementById(containerId);
-      container.innerHTML = "";
+function getMovies(endpoint, containerId) {
+    fetch(`${baseUrl}${endpoint}?api_key=${apiKey}&language=pt-BR`)
+        .then(res => res.json())
+        .then(data => {
+            const container = document.getElementById(containerId);
+            container.innerHTML = "";
 
-      data.results.forEach(movie => {
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = `
-          <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
-        `;
-        container.appendChild(div);
-      });
-    });
+            data.results.forEach(movie => {
+                const div = document.createElement("div");
+                div.classList.add("movie");
+
+                div.innerHTML = `
+                    <a href="details.html?id=${movie.id}">
+                        <img src="${imageUrl + movie.poster_path}" alt="${movie.title}">
+                    </a>
+                `;
+
+                container.appendChild(div);
+            });
+        });
 }
 
-fetchCategory("/movie/popular", "popular");
-fetchCategory("/movie/now_playing", "now_playing");
-fetchCategory("/movie/top_rated", "top_rated");
+getMovies("/movie/popular", "popular");
+getMovies("/movie/now_playing", "now_playing");
+getMovies("/movie/top_rated", "top_rated");
